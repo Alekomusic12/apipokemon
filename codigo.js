3c04d3552fae742b1585968fcb9aa7b25ce849b1
Alekomusic12 <117124383+Alekomusic12@users.noreply.github.com>
2023-02-14 20:05:00 -0500

    versionmejorada2

--- codigo.js
+++ codigo.js
@@ -1,43 +1,50 @@
-function pokemonApi(urlAPI = 'https://pokeapi.co/api/v2/pokemon/') {
-    document.querySelector("#datosweb").innerHTML = ""
-    let peticion = fetch(urlAPI)
-    peticion.then(res => res.json())
-        .then(data => {
-            for (const picachu of data.results) {
-                fetch(picachu.url)
-                    .then(res2 => res2.json())
-                    .then(lospokemon => {
-                        lospokemon.stats.forEach((atributo, posicion) => {
-                            if (posicion == 3) {
+const apiUrl = 'https://pokeapi.co/api/v2/pokemon';
+const limit = 6;
+let offset = 0;
 
+function loadPokemon(offset) {
+  const paginationContainer = document.getElementById('pagination-container');
+  paginationContainer.innerHTML = '';
 
-                                document.querySelector("#datosweb").innerHTML += `
-                                        
-                                        <div class="col">
-                                        <div class="card mt-5 mb-0">
-                                        <div class="card-body cartas">
-                                        <img src="${lospokemon.sprites.other.home.front_default}" class="w-50 ms-5 "> 
-                                        <h3 class="card-title justify-content-center align-content-center text-center" >${picachu.name}</h3>
-                                        <h6 class="card-text justify-content-center align-content-center text-center">${atributo.stat.name}</h6>
-                                        </div>
-                                        </div>
-                                        </div>
-                                        `
-                            };
-                        });
-                    })
-            }
-            siguienteAnterior(data.next, data.previous)
-        })
+  fetch(`${apiUrl}?limit=${limit}&offset=${offset}`)
+    .then(response => response.json())
+    .then(data => {
+      const pokemonContainer = document.getElementById('pokemon-container');
+      pokemonContainer.innerHTML = '';
+
+      data.results.forEach(pokemon => {
+        const pokemonElement = document.createElement('div');
+        pokemonElement.classList.add('pokemon-card');
+        const nameElement = document.createElement('p');
+        const imageElement = document.createElement('img');
+        imageElement.classList.add('pokemon-image');
+
+        nameElement.textContent = pokemon.name;
+        pokemonElement.appendChild(nameElement);
+
+        fetch(pokemon.url)
+          .then(response => response.json())
+          .then(data => {
+            const imageUrl = data.sprites.front_default;
+            imageElement.src = imageUrl;
+          })
+          .catch(error => console.log(error));
+
+        pokemonElement.appendChild(imageElement);
+        pokemonContainer.appendChild(pokemonElement);
+      });
+
+      const previousButton = document.createElement('button');
+      previousButton.textContent = 'Previous';
+      previousButton.addEventListener('click', () => loadPokemon(offset - limit));
+      paginationContainer.appendChild(previousButton);
+
+      const nextButton = document.createElement('button');
+      nextButton.textContent = 'Next';
+      nextButton.addEventListener('click', () => loadPokemon(offset + limit));
+      paginationContainer.appendChild(nextButton);
+    })
+    .catch(error => console.log(error));
 }
 
-
-function siguienteAnterior(data_next, data_previous) {
-    let disabledBotonNext = (data_next == null) ? "disabled" : "";
-    let disabledBotonPrev = (data_previous == null) ? "disabled" : "";
-    document.querySelector("#anteSig").innerHTML = `
-                            <button class="btn btn-dark  fs-1" ${disabledBotonPrev} onclick="pokemonApi('${data_previous}')">Anterior</button>
-                            <button class="btn btn-dark  fs-1" ${disabledBotonNext} onclick="pokemonApi('${data_next}')">Siguiente</button>`
-}
-
-pokemonApi()
+loadPokemon(offset);
